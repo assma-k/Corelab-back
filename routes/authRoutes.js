d8b90abcd61360a665dfa94971fdc112b9062f02
Assma <assma.kebbous@epitech.eu>
2026-06-12 13:20:25 +0200

    git commit -m "Feat: Ajout de la route de mise à jour du mot de passe avec gestion du firstLogin"

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login, updatePassword } = require('../controllers/authController');
+
+const { verifyToken } = require('../middlewares/authMiddleware');
 
 router.post('/register', register);
 
 router.post('/login', login);
+
+router.post('/updatePassword', verifyToken, updatePassword)
 
 module.exports = router;
